ed7dcf595de3abfc4be9cb497d7b9e7dd48d59f1
BobbyAxelrods <muhdshafiqsafian@gmail.com>
2023-06-19 20:06:41 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# xlsx_to_json
+Simple apps to convert xlsx files to json with option nrows for speeds , dtypes as string to avoid missing leading 000 when conversion. 
+This was created to assist web dev to convert for testing table builder. 
+
+Upcoming Feature 
+1. Add upload module and buttons to run the scripts and save the output for user to upload 
---
+++ XLSX_JSON_CONVERTER.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from queue import Empty
+import openpyxl
+import numpy as np
+import os 
+import json
+import csv 
+import pandas as pd
+from csv import DictReader
+versioning = pd.__version__
+
+working_path = os.getcwd()
+csv_folder = os.path.join(working_path,'csv_folder')
+excel_folder = os.path.join(working_path,'excel_folder')
+output_folder = os.path.join(working_path,'output')
+print("Running Version Pandas :%s"%(versioning))
+print('Im ready to convert sir')
+
+
+xlsx_fileList = [f for f in os.listdir(excel_folder) if f.endswith('.xlsx')]
+csv_fileList = [f for f in os.listdir(csv_folder) if f.endswith('.csv')]
+
+if xlsx_fileList:
+    print(f'{xlsx_fileList} ready to be converted')
+else:
+    print('No excel files available')
+
+if csv_fileList:
+    print(f'{csv_fileList} ready to be converted')
+else:
+    print('No csv files available')
+
+if xlsx_fileList: 
+    for sheet in xlsx_fileList:
+        fileNameExcel = str(sheet)[:-5]    
+        #define total row to read , if all remove nrows=xxxx,
+        #dtypes added to avoid missing leading zeros, hence added dtype =str assuming all values a string 
+        df = pd.read_excel(excel_folder+'/'+sheet,nrows=10, dtype=str) 
+        os.chdir(output_folder)
+        df.to_json("{}.json".format(fileNameExcel), orient='records', indent=2)
+
+        jsonlist = [f for f in os.listdir(output_folder) if f.endswith('json')]
+        print('Conversion to json {} success'.format(sheet))
+else: 
+    print('No excel to be converted')
+
+if csv_fileList:   
+    for sheet in fileList:
+        fileNameExcel = str(sheet)[:-5]    
+        df = pd.read_csv(csv_folder+'/'+sheet,nrows=10, dtype=str)
+        os.chdir(output_folder)
+        df.to_json("{}.json".format(fileNameExcel), orient='records', indent=2)
+
+        jsonlist = [f for f in os.listdir(output_folder) if f.endswith('json')]
+        print('Conversion to json {} success'.format(sheet))
+    
+else: 
+    print('No csv to be converted')
+    
+df3 = pd.DataFrame (jsonlist, columns = ['List of json Available '])
+
+df3
+
